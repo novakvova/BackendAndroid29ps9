5946cbaeaa687dfaee61ee62f43f355bc2687d6f
novakvova <novakvova@gmail.com>
2019-04-21 13:10:59 +0300

    Begin login

---
+++ BackendAndroid/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackendAndroid.BLL.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendAndroid.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    //[ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody]Credentials credentials)
+        {
+            return Ok();
+        }
+    }
+}
